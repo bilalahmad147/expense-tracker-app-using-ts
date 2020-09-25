ca7c642ae273f31fcc47a54c1315ccce2ab276d2
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-25 13:14:44 +0500

    starting todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import History from './Components/History';
-import ExpenseDetail from './Components/ExpenseDetail';
-import Expense from './Components/Expense';
-import Balance from './Components/Balance';
 
 function App() {
   return (
     <div className="App">
-      <Balance />
-      <ExpenseDetail />
-      <h2>HISTORY</h2>
-      <History />
-      <Expense />
+      <h1>hello world</h1>
     </div>
   );
 }
